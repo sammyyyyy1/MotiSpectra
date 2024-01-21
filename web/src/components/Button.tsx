b77d3e580389e029cdedf7d53214df3cf1240f9a
Steven Wu <wusteven815@gmail.com>
2024-01-20 21:44:51 -0500

    style: clean code

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -3,13 +3,13 @@ interface Props {
   children: string;
   disabled?: boolean;
   onClick: () => void;
-};
+}
 
 export default function Button({ children, className = "", disabled = false, onClick }: Props) {
   return (
     <button
-      className={`${disabled ? "bg-gray-500 text-white" : "bg-primary text-on-primary"} py-2.5 px-6 
-                  rounded-full text-label-large ${className}`}
+      className={`${disabled ? "bg-gray-500 text-white" : "bg-primary text-on-primary"} transition-colors 
+                  py-2.5 px-6 rounded-full text-label-large ${className}`}
       disabled={disabled}
       onClick={onClick}
     >
